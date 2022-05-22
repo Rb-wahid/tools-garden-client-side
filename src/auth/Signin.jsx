ec797d9c370a562140a5c796b6a4fba5bff81075
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-22 15:55:31 +0600

    design Signin component

---
+++ src/auth/Signin.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable no-useless-escape */
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import AuthError from "../components/AuthError";
+
+const Signin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <section className="hero h-screen">
+      <div class=" card flex-shrink-0 w-full max-w-sm  shadow-2xl bg-base-100">
+        <div class="card-body">
+          <h2 class="card-title uppercase justify-center text-secondary ">
+            Sign in
+          </h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                class="input input-bordered"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
+                  pattern: {
+                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                    message: "Enter a valid email",
+                  },
+                })}
+              />
+              {errors.email?.type === "required" && (
+                <AuthError>{errors.email.message}</AuthError>
+              )}
+              {errors.email?.type === "pattern" && (
+                <AuthError>{errors.email.message}</AuthError>
+              )}
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                class="input input-bordered"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is required",
+                  },
+                })}
+              />
+              {errors.password?.type === "required" && (
+                <AuthError>{errors.password.message}</AuthError>
+              )}
+              <label class="label mt-1 ">
+                <Link
+                  to="#!"
+                  class="label-text-alt link link-hover text-blue-400 text-sm"
+                >
+                  Forgot password?
+                </Link>
+                <Link
+                  to={"/signup"}
+                  class="label-text-alt link link-hover text-blue-400 text-sm uppercase"
+                >
+                  Create new account
+                </Link>
+              </label>
+            </div>
+            <div class="form-control mt-6">
+              <button type="submit" class="btn btn-primary">
+                Signin
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signin;
