939298b64aad0581e184f6fa6bb7afa7f76655b0
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-28 16:27:44 +0600

    Implement Dashboard component

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+  return (
+    <section className="">
+      <div class="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col items-center ">
+          <h2 className="text-center font-bold text-2xl text-Secondary my-6">
+            Welcome to Dashboard
+          </h2>
+          <Outlet />
+          <label
+            for="my-drawer-2"
+            class="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
+        </div>
+        <div class="drawer-side">
+          <label for="my-drawer-2" class="drawer-overlay"></label>
+          <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+            <li>
+              <NavLink to="/dashboard">My Profile</NavLink>
+            </li>
+            <li>
+              <NavLink to="my-orders">My Orders</NavLink>
+            </li>
+            <li>
+              <NavLink to="add-review">Add Review</NavLink>
+            </li>
+            <li>
+              <NavLink to="manage-all-orders">Manage All Orders</NavLink>
+            </li>
+            <li>
+              <NavLink to="add-product">Add Product</NavLink>
+            </li>
+            <li>
+              <NavLink to="make-admin">Make Admin</NavLink>
+            </li>
+            <li>
+              <NavLink to="manage-products">Manage Products</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
