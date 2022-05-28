20a889d34c40a88dc3ad62f5c8cdba4ee5f6e01e
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-28 16:52:57 +0600

    Implement Header component

---
+++ src/pages/Home/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import video from "../../assets/video//rto-hero-v1 .mp4";
+const Header = () => {
+  return (
+    <header>
+      <video
+        src={video}
+        className="w-screen"
+        controls="controls"
+        autoPlay
+        loop
+        muted
+      />
+      <div class="card w-96 shadow-xl top-96 left-72 absolute bg-transparent bg-base-100">
+        <div class="card-body">
+          <h2 class="text-5xl uppercase font-bold text-warning">
+            Rule the outdoors
+          </h2>
+          <p className="text-xl font-semibold">
+            Discover the world’s largest professional cordless outdoor power
+            equipment system.
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
